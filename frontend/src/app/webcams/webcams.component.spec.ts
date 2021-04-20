0fe01265ca227c9de3d2ba2fb3a20f783b5d7668
Mehmet Karatay <m.r.karatay@gmail.com>
2021-04-20 08:39:36 +0100

    chore(frontend): Generated Webcams component.

---
+++ frontend/src/app/webcams/webcams.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WebcamsComponent } from './webcams.component';
+
+describe('WebcamsComponent', () => {
+  let component: WebcamsComponent;
+  let fixture: ComponentFixture<WebcamsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WebcamsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WebcamsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/webcams/webcams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webcams',
+  templateUrl: './webcams.component.html',
+  styleUrls: ['./webcams.component.css']
+})
+export class WebcamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
