d08599658cac79dd8d68ba78ec8c8640ee6272e5
Mehmet Karatay <m.r.karatay@gmail.com>
2021-05-05 10:43:16 +0100

    refac(backend): pwImport() is inner function of test_dbConfig()

--- backend/tests/entities/test_entity.py
+++ backend/tests/entities/test_entity.py
@@ -3,17 +3,17 @@ from src.entities.entity import *
 
 from configparser import ConfigParser
 
-def pwImport():
-		config = ConfigParser()
-		config.read(CONFIG)
-		return config['database']['pw']
-
 class Test_Entity(unittest.TestCase):
 
 		# This test reads in the same password as entity.py so should always pass
 		# but it there is a mistake in the code of EITHER src/ or test/ then it will
 		# highlight a problem. I am trying to avoid hard-coding the pw into the repo.
 		def test_dbConfig(self):
+				def pwImport():
+						config = ConfigParser()
+						config.read(CONFIG)
+						return config['database']['pw']
+
 				want = pwImport()
 				got = getDbPw()
 				msgLen = 'Password has zero characters'
